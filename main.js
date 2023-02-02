66159d3da766898481d0ebebc8a68446c3e76c4e
WUT2022 <pmalm1988@gmail.com>
2023-02-02 12:23:32 +0100

    fått in bild att fungera på sidan

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 // document.body.innerHTML = "testing"
 // UNlVhtIyqAzN6ipcOKa5AzL6yIbIQ2BBMXgDKvcb
-async function getNASA() {
-    const response = await fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=UNlVhtIyqAzN6ipcOKa5AzL6yIbIQ2BBMXgDKvcb')
-    const nasa = await response.json()
-    console.log(nasa)
-    document.getElementById('test').innerHTML += nasa.photos
 
 
+
+// async function getNASA() {
+ //   const response = await fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=UNlVhtIyqAzN6ipcOKa5AzL6yIbIQ2BBMXgDKvcb')
+ fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=UNlVhtIyqAzN6ipcOKa5AzL6yIbIQ2BBMXgDKvcb')  
+.then(response => response.json())
+.then(data => {
+    pictures = data.photos
+    console.log(data.photos[2].img_src)
+    document.body.innerHTML += `<img src="${data.photos[2].img_src}" alt="Girl in a jacket" width="500" height="600">` 
+
+})
+
+//     const pic1 = nasa.photos[5].img_src
+//     console.log(pic1)
+//     // document.getElementById('test').innerHTML += `${pic1}`
+//     document.body.innerHTML += `${pic1}`
+
+// }
+
+function makeImg(img) {
+    return `<img src="${img}" alt="Girl in a jacket" width="500" height="600"> `
 }
 
-getNASA()
+//const result = makeImg(get[0])
+
+//makeImg(pic1)
+makeImg()
+//getNASA()
